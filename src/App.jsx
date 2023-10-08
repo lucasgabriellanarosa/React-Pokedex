c3a4abea0df4c34a748bbcbc49daa1a947ade57a
Lucas Gabriel de Lana Rosa <lucas.gabriel.lr2112@gmail.com>
2023-10-08 19:52:57 -0300

    swiper

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,20 @@ import './App.css'
 import axios from "axios";
 import { PokemonCard } from './components/PokemonCard/PokemonCard';
 import { Header } from './components/Header/Header';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Grid } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/grid';
+
 function App() {
 
   const [pokemons, setPokemons] = useState([])
 
   const getPokemonData = async () => {
     let endpoints = [];
-    for (let i = 1; i < 13; i++) {
+    for (let i = 1; i < 152; i++) {
       endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
     }
     axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((res) => setPokemons(res));
@@ -24,11 +31,25 @@ function App() {
       <Header />
 
 
-      <ul className='pokemonContainer'>
+    <main className='swiperContainer'>
+      <Swiper      
+      className='pokemonContainer'
+      modules={[Navigation, Pagination, Grid]}
+      spaceBetween={25}
+      slidesPerView={4}
+      navigation
+      pagination={{ clickable: true }}
+      grid={{
+        fill: 'row',
+        rows: 3,
+      }}
+      >
         {
-          pokemons.map((pokemon) => <PokemonCard key={pokemon.data.id} pokemonData={pokemon.data}/> )
+          pokemons.map((pokemon) => <SwiperSlide key={pokemon.data.id}><PokemonCard pokemonData={pokemon.data}/></SwiperSlide>)
+
         }
-      </ul>
+      </Swiper>
+    </main>
     </div>
 
 
